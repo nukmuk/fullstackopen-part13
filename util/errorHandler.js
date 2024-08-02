13c155eb18eab723ed17de07170fa927381ffe7c
nukmuk <10235825+nukmuk@users.noreply.github.com>
2024-08-02 16:25:47 +0300

    13.9 done (& 13.10)

--- util/errorHandler.js
+++ util/errorHandler.js
@@ -1,6 +1,9 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err.message);
 
+  // if (err.name === "SequelizeValidationError")
+  //   res.status(400).send({ error: err.errors[0].message });
+
   res.status(400).send({ error: err.message });
 
   next(err);
